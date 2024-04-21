231cafe190ce0d15327e70488c7ce1d3376fdf83
Lauren Peraza <lauren.peraza16@gmail.com>
2024-04-20 22:10:51 -0400

    demographics and PassengerAgent fixes

--- Agents/PassengerAgent.py
+++ Agents/PassengerAgent.py
@@ -1,5 +1,5 @@
 from Agents.Agent import Agent
-
+from Agents.agent_tools import distance_between
 
 class PassengerAgent(Agent):
     """
@@ -7,8 +7,13 @@ class PassengerAgent(Agent):
     
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, profile, travel_goal):
+        self.profile = profile
+        self.travel_goal = travel_goal
+        self.route = None
+        self.waiting_time = 0
+        self.nearest_bus_stop = None
+        self.suitable_stops = None
 
     def decide_action(self, environment):
         """
@@ -20,4 +25,107 @@ class PassengerAgent(Agent):
         Returns:
             str: The action to take.
         """
+        current_location = environment['current_location']
+        bus_stops = environment['bus_stops']
+        buses = environment['buses']
+        gazelles = environment['gazelles']
+        city_map = environment['city_map']
+        
+        if self.decide_boarding_vehicle(environment["buses_at_stop"], environment["gazelles_at_stop"], buses, gazelles):
+                self.waiting_time = 0
+                return "board vehicle"
+            
+        elif self.waiting_time < self.profile['max_waiting_time']:
+            self.waiting_time += 1
+            return "wait at stop"
+        
+        elif environment["at_bus_stop"] and self.waiting_time >= self.profile['max_waiting_time']:
+            return "search alternative transport"
+        
+        elif environment["on_bus"]:
+            if environment["current_location"] == self.travel_goal:
+                return "arrive destination"
+            else:
+                self.monitor_environment(environment)
+                return "continue journey"
+            
+        elif not environment["waiting_at_stop"] and not environment["walking"]:
+            self.decide_destination_stop(current_location, environment['bus_stops'], buses)
+            return f"walk to {self.nearest_bus_stop}"
+
+    def decide_boarding_vehicle(self, buses_at_stop, gazelles_at_stop):
+        
+        if not buses_at_stop and not gazelles_at_stop:
+            return False
+
+        suitable_vehicles = []
+
+        for bus in buses_at_stop:
+            if bus.current_capacity < bus.max_capacity and self.is_vehicle_convenient(bus):
+                suitable_vehicles.append(bus)
+
+        if self.profile.get("willing_to_take_gazelle", False):
+            for gazelle in gazelles_at_stop:
+                if gazelle.current_capacity < gazelle.max_capacity and self.is_vehicle_convenient(gazelle):
+                    suitable_vehicles.append(gazelle)
+
+        if not suitable_vehicles:
+            return False
+
+        best_vehicle = self.select_best_vehicle(suitable_vehicles)
+
+        if best_vehicle:
+            return True
+        else:
+            return False
+    
+    def find_nearest_bus_stop(self, current_location, bus_stops):
+        min_distance = float('inf')
+        nearest_stop = None
+
+        for stop_id, stop_location in bus_stops.items():
+            distance = distance_between(current_location, stop_location)
+            if distance < min_distance:
+                min_distance = distance
+                nearest_stop = stop_id
+
+        return nearest_stop
+
+    def search_alternative_transport(self):
+        #buscar alternativas de transporte?
         pass
+
+    def decide_destination_stop(self, buses, current_location):
+
+        self.suitable_stops = self.find_suitable_stops(self.travel_goal, buses)
+        self.nearest_bus_stop = self.find_nearest_bus_stop(current_location, self.suitable_stops)
+   
+
+    def select_best_vehicle(self, vehicles):
+        
+        if not vehicles:
+            return None
+        
+        best_vehicle = None
+        min_distance = float('inf')
+        
+        for vehicle in vehicles:
+        
+            for stop_node in vehicle.route:
+                distance_to_destination = distance_between(stop_node, self.travel_goal)
+
+                if distance_to_destination < min_distance_to_destination:
+                    min_distance_to_destination = distance_to_destination
+                    best_vehicle = vehicle
+        
+        return best_vehicle
+    
+    def find_suitable_stops(self, travel_goal, buses):
+
+        suitable_stops = set()
+
+        for bus in buses:
+            if self.is_vehicle_convenient(bus) and travel_goal in bus.route:
+                suitable_stops.update(bus.route)
+
+        return list(suitable_stops)
--- Demographics/generator.py
+++ Demographics/generator.py
@@ -5,58 +5,107 @@ class Person:
         self.attributes = attributes
 
 class DemographicGenerator:
-
     def __init__(self):
-
         self.demographic_distributions = {
             'age': {'0-4': 0.043, '5-9': 0.052, '10-14': 0.049, '15-19': 0.049, '20-24':0.06, '25-29':0.062, '30-34':0.074, '35-39':0.067, '40-44':0.053, '45-49':0.069, '50-54':0.083, '55-59':0.096, '60-64':0.07, '65+':0.166},
             'gender': {'Male': 0.47, 'Female': 0.53},
-            'location': {'Playa': 0.083, 'Plaza de la Revolución': 0.065, 'Centro Habana': 0.061, 'La Habana Vieja': 0.037, 'Regla': 0.02, 'La Habana del Este': 0.081, 'Guanabacoa': 0.06, 'San Miguel del Padrón': 0.075, 'Diez de Octubre': 0.093, 'Cerro': 0.058, 'Marianao': 0.062, 'La Lisa': 0.068, 'Boyeros': 0.095, 'Arroyo Naranjo': 0.096, 'Cotorro': 0.038}
+            'location': {'Playa': 0.083, 'Plaza de la Revolución': 0.065, 'Centro Habana': 0.061, 'La Habana Vieja': 0.037, 'Regla': 0.02, 'La Habana del Este': 0.081, 'Guanabacoa': 0.06, 'San Miguel del Padrón': 0.075, 'Diez de Octubre': 0.093, 'Cerro': 0.058, 'Marianao': 0.062, 'La Lisa': 0.068, 'Boyeros': 0.095, 'Arroyo Naranjo': 0.096, 'Cotorro': 0.038},
+            'money': {
+                '0-4': {'low': 0.9, 'medium': 0.05, 'high': 0.05},
+                '5-9': {'low': 0.6, 'medium': 0.3, 'high': 0.1},
+                '10-14': {'low': 0.3, 'medium': 0.6, 'high': 0.1},
+                '15-19': {'low': 0.6, 'medium': 0.3, 'high': 0.1},
+                '20-24': {'low': 0.3, 'medium': 0.6, 'high': 0.1},
+                '25-29': {'low': 0.1, 'medium': 0.8, 'high': 0.1},
+                '30-34': {'low': 0.1, 'medium': 0.8, 'high': 0.1},
+                '35-39': {'low': 0.1, 'medium': 0.8, 'high': 0.1},
+                '40-44': {'low': 0.1, 'medium': 0.8, 'high': 0.1},
+                '45-49': {'low': 0.1, 'medium': 0.8, 'high': 0.1},
+                '50-54': {'low': 0.1, 'medium': 0.8, 'high': 0.1},
+                '55-59': {'low': 0.1, 'medium': 0.8, 'high': 0.1},
+                '60-64': {'low': 0.1, 'medium': 0.8, 'high': 0.1},
+                '65+': {'low': 0.4, 'medium': 0.5, 'high': 0.1},
+            },
+            #hacer travel_goal más accurated
+            'travel_goal': {'Playa': 0.1, 'Plaza de la Revolución': 0.1, 'Centro Habana': 0.1, 'La Habana Vieja': 0.1, 'Regla': 0.1, 'La Habana del Este': 0.1, 'Guanabacoa': 0.1, 'San Miguel del Padrón': 0.1, 'Diez de Octubre': 0.1, 'Cerro': 0.1, 'Marianao': 0.1, 'La Lisa': 0.1, 'Boyeros': 0.1, 'Arroyo Naranjo': 0.1, 'Cotorro': 0.1},
+            'max_waiting_time': {'low': {'low': 0.9, 'medium': 0.6, 'high': 0.3},
+                             'medium': {'low': 0.05, 'medium': 0.3, 'high': 0.6},
+                             'high': {'low': 0.05, 'medium': 0.1, 'high': 0.1}}
         }
 
-    def add_distribution(self, attribute, distribution):
-
-        if isinstance(distribution, dict):
-            self.demographic_distributions[attribute] = distribution
-        
-        else:
-            raise ValueError("Distribution must be a dictionary.")
-
     def alter_distribution(self, attribute, new_distribution):
-       
+
         if attribute in self.demographic_distributions:
+        
             if isinstance(new_distribution, dict):
                 self.demographic_distributions[attribute].update(new_distribution)
-       
+        
             else:
                 raise ValueError("New distribution must be a dictionary.")
-       
+        
         else:
             raise ValueError(f"No distribution defined for attribute '{attribute}'")
 
-    def generate_demographic_data(self, attribute):
+    def generate_demographic_data(self, attribute, current_age=None, current_money_category=None):
         
         distribution = self.demographic_distributions.get(attribute)
         
-        if distribution:
-            categories = list(distribution.keys())
-            weights = list(distribution.values())
-            category = random.choices(categories, weights=weights)[0]
-            return category
+        if not distribution:
+            raise ValueError(f"No distribution defined for attribute '{attribute}'")
+
+        if attribute in ['age', 'gender', 'location', 'travel_goal']:
+            return random.choices(list(distribution.keys()), weights=list(distribution.values()))[0]
+        
+        elif attribute == 'money':
+        
+            if not current_age:
+                raise ValueError("Current age must be provided for generating money category.")
+        
+            age_distribution = self.demographic_distributions['age']
+            age_category = current_age if current_age in age_distribution else random.choices(list(age_distribution.keys()), weights=list(age_distribution.values()))[0]
+            money_distribution = self.demographic_distributions['money'].get(age_category)
+        
+            if not money_distribution:
+                raise ValueError(f"No money distribution defined for age category '{age_category}'")
+        
+            return random.choices(list(money_distribution.keys()), weights=list(money_distribution.values()))[0]
+        
+        elif attribute == 'max_waiting_time':
+        
+            if not current_money_category:
+                raise ValueError("Current money category must be provided for generating waiting time.")
+        
+            max_waiting_time_distribution = self.demographic_distributions['max_waiting_time'].get(current_money_category.lower())
+        
+            if not max_waiting_time_distribution:
+                raise ValueError(f"No waiting time distribution defined for money category '{current_money_category}'")
+        
+            return random.choices(['low', 'medium', 'high'], weights=list(max_waiting_time_distribution.values()))[0]
         
         else:
-            raise ValueError(f"No distribution defined for attribute '{attribute}'")
+            raise ValueError("Attribute not supported for demographic data generation.")
 
     def generate_person_list(self, sample_size):
         
         person_list = []
         
         for _ in range(sample_size):
+        
             attributes = {}
+            current_age = None
+            current_money_category = None
+        
             for attribute in self.demographic_distributions.keys():
-                attributes[attribute] = self.generate_demographic_data(attribute)
-            
+                if attribute == 'age':
+                    attributes[attribute] = current_age = self.generate_demographic_data(attribute)
+                elif attribute == 'money':
+                    attributes[attribute] = current_money_category = self.generate_demographic_data(attribute, current_age=current_age)
+                elif attribute == 'max_waiting_time':
+                    attributes[attribute] = self.generate_demographic_data(attribute, current_money_category=current_money_category)
+                else:
+                    attributes[attribute] = self.generate_demographic_data(attribute)
+
             person = Person(attributes)
             person_list.append(person)
-        
+
         return person_list
