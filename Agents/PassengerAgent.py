ae7c71c9594bfafb4c3f932568bc83ce45448dcb
Lauren Peraza <lauren.peraza16@gmail.com>
2024-04-28 14:06:13 -0400

    change PassengerAgent structure

--- Agents/PassengerAgent.py
+++ Agents/PassengerAgent.py
@@ -1,18 +1,37 @@
+from enum import Enum
+from queue import PriorityQueue
 from Agents.Agent import Agent
+from routing import routing
 
+class PlanType(Enum):
+    WORK = 3
+    RETURN_HOME = 2
+    RANDOM_TRAVEL = 1
+
+class Plan:
+    def __init__(self, goal, plan_type):
+        self.goal = goal
+        self.importance = plan_type.value
+
+    def __lt__(self, other):
+        return self.importance < other.importance
+    
 class PassengerAgent(Agent):
     """
     Represents a passenger agent.
     
     """
+    def __init__(self, profile, plans, home_block, workplace_block, current_block):
 
-    def __init__(self, profile, travel_goal):
         self.profile = profile
-        self.travel_goal = travel_goal
-        self.route = None
-        self.waiting_time = 0
-        self.nearest_bus_stop = None
-        self.suitable_stops = None
+
+        self.plans = PriorityQueue() 
+        for plan in plans:
+            self.plans.put(plan)
+        
+        self.home_block = home_block
+        self.workplace_block = workplace_block
+        self.current_block = current_block
 
     def decide_action(self, environment):
         """
@@ -26,8 +45,8 @@ class PassengerAgent(Agent):
         """
 
         if not environment["waiting"] and not environment["at_vehicle"] and not environment["walking"]:
-            target_stop = self.explore_nearby_nodes(environment["city_map"], 1000)
-            self.route = self.get_route_to_stop(target_stop)
+            target_stop = self.explore_nearby_nodes(environment["city_map"], 1000, environment["current_location"])
+            self.route = self.get_route_to_stop(target_stop, environment["city_map"])
             return "walk to stop"
         
         elif environment["waiting"]:
@@ -56,35 +75,36 @@ class PassengerAgent(Agent):
                 return "continue journey"
             
 
-    def explore_nearby_nodes(self, map, radius):
+    def explore_nearby_nodes(self, map, radio, current_location):
 
-        nearby_nodes = map.get_nodes_by_radius(self.current_location, radius)
+        blocks_of_interest = routing.blocks_in_radio(map, radio, current_location)
         
         max_score = float('-inf')
-        best_node = None
-        for node in nearby_nodes:
-            score = self.calculate_node_score(node) 
+        best_block = None
+
+        for block in blocks_of_interest:
+            score = self.calculate_block_score(map, block, current_location) 
             if score > max_score:
                 max_score = score
-                best_node = node
+                best_block = block
 
-        return best_node
+        return best_block
 
-    def calculate_node_score(self, block):
+    def calculate_block_score(self, map, block, current_location):
         score = 0 
 
-        distance_to_passenger = self.current_location.length_to(block.location)
+        distance_to_passenger = current_location.length_to(block.location)
         max_score = 3
         target_distance = 500  
         slope = 1 / target_distance  
 
         score = max(0, slope * distance_to_passenger + max_score)
 
-        valid_routes = self.get_valid_routes(block)
+        valid_routes = routing.get_routes(map, block.location, self.travel_goal, 1000)
 
         for route in valid_routes:
 
-            if route.mode == "gazelle":
+            if route in map.gazelle_routes:
                 if self.profile.money >= 5:
                     score += 1  
                 if route.outbound_route[0] == block:
@@ -95,3 +115,9 @@ class PassengerAgent(Agent):
                 score += 2 
         
         return score
+    
+    def get_route_to_stop(self, target_stop, map):
+        pass
+
+    def decide_boarding_vehicle(self, current_location):
+        pass
