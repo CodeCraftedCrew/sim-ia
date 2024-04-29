db037ff3dcecce97c9f4b43b80073bacc11a06f9
Lauren Peraza <lauren.peraza16@gmail.com>
2024-04-28 20:54:47 -0400

    add calculate route time in PassengerAgent

--- Agents/PassengerAgent.py
+++ Agents/PassengerAgent.py
@@ -10,12 +10,10 @@ def time_to_minutes(time_str):
     total_minutes = (hours * 60) + minutes
     return total_minutes
 
-
 class PlanType(Enum):
     GO_TO_WORK = 3
     RETURN_HOME = 2
     RANDOM_TRAVEL = 1
-
 
 class Plan:
     def __init__(self, time, goal, plan_type):
@@ -24,7 +22,12 @@ class Plan:
 
     def __lt__(self, other):
         return self.importance < other.importance
-
+    
+class PassengerState(Enum):
+    INITIAL_STATE = 1
+    WALKING = 2
+    WAITING = 3
+    AT_VEHICLE = 4
 
 class PassengerAgent(Agent):
     """
@@ -44,6 +47,9 @@ class PassengerAgent(Agent):
         self.home_block = home_block
         self.workplace_blocks = workplace_blocks
         self.current_block = current_block
+
+        self.state = PassengerState.INITIAL_STATE
+        self.expected_waiting_time = 15
 
     @staticmethod
     def get_schedule(profile):
@@ -72,6 +78,11 @@ class PassengerAgent(Agent):
         ]
 
         return plans
+    
+    def update_expected_waiting_time(self, waiting_time):
+        smoothing_factor = 0.4 
+        new_expected_waiting_time = smoothing_factor * waiting_time + (1 - smoothing_factor) * self.expected_waiting_time
+        self.expected_waiting_time = round(new_expected_waiting_time)
 
     def decide_action(self, environment):
         """
@@ -84,13 +95,14 @@ class PassengerAgent(Agent):
             str: The action to take.
         """
 
-        if not environment["waiting"] and not environment["at_vehicle"] and not environment["walking"]:
-            target_stop = self.explore_nearby_nodes(environment["city_map"], 1000, environment["current_location"])
-            self.route = self.get_route_to_stop(target_stop, environment["city_map"])
+        if self.state not in [PassengerState.WAITING, PassengerState.WALKING, PassengerState.AT_VEHICLE]:
+            route = self.explore_routes(environment["city_map"], 1000, self.plans[0].goal)
+            self.state = PassengerState.WALKING
             return "walk to stop"
 
-        elif environment["waiting"]:
-            if self.decide_boarding_vehicle(environment["current_location"]):
+        elif self.state == PassengerState.WAITING:
+            if self.decide_boarding_vehicle(self.current_block):
+                self.update_expected_waiting_time(self.waiting_time)
                 self.waiting_time = 0
                 return "board vehicle"
             else:
@@ -100,9 +112,10 @@ class PassengerAgent(Agent):
                 else:
                     return "search alternative transport"
 
-        elif environment["walking"]:
+        elif self.state == PassengerState.WALKING:
             if environment["current_location"] == self.route[-1]:
                 return "arrive to stop"
+
             elif environment["current_location"] == self.travel_goal:
                 return "arrive to destination"
             else:
@@ -114,49 +127,36 @@ class PassengerAgent(Agent):
             else:
                 return "continue journey"
 
-    def explore_nearby_nodes(self, map, radio, current_location):
+    def explore_routes(self, map, radio, goal):
 
-        blocks_of_interest = routing.blocks_in_radio(map, radio, current_location)
+        possible_routes = routing.get_routes(map, self.current_block, goal, radio)
 
         max_score = float('-inf')
-        best_block = None
+        best_route = None
 
-        for block in blocks_of_interest:
-            score = self.calculate_block_score(map, block, current_location)
+        for route in possible_routes:
+            score = self.calculate_route_time(map, route)
             if score > max_score:
                 max_score = score
-                best_block = block
+                best_route = route
 
-        return best_block
+        return best_route
+    
+    def calculate_route_time(self, map, route):
+        time = 0
 
-    def calculate_block_score(self, map, block, current_location):
-        score = 0
+        time += route.total_time
 
-        distance_to_passenger = current_location.length_to(block.location)
-        max_score = 3
-        target_distance = 500
-        slope = 1 / target_distance
+        previous_action = None
+        for action, blocks in route:
+            if previous_action and previous_action != action != "walk":
+                time += self.expected_waiting_time
+            elif previous_action == "walk" and action != "walk":
+                time += self.expected_waiting_time
+            previous_action = action
+        
+        return time
 
-        score = max(0, slope * distance_to_passenger + max_score)
-
-        valid_routes = routing.get_routes(map, block.location, self.travel_goal, 1000)
-
-        for route in valid_routes:
-
-            if route in map.gazelle_routes:
-                if self.profile.money >= 5:
-                    score += 1
-                if route.outbound_route[0] == block:
-                    score += 1
-            else:
-                if route.outbound_route[0] == block:
-                    score += 1
-                score += 2
-
-        return score
-
-    def get_route_to_stop(self, target_stop, map):
-        pass
 
     def decide_boarding_vehicle(self, current_location):
         pass
